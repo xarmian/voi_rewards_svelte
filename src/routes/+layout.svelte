7c3992b14ed339cdf67a73b75c52106e1be5a097
David Runion <david.runion@imedemr.com>
2023-10-19 21:57:58 -0400

    analytics test 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,10 @@
 	// import need to init tailwind
 	import '../app.css';
 	import '@fortawesome/fontawesome-free/css/all.min.css'
-	import { webVitals } from '$lib/vitals';
-	import { page } from '$app/stores';
-	import { browser } from '$app/environment';
 
-	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-
-	$: if (browser && analyticsId) {
-		webVitals({
-			path: $page.url.pathname,
-			params: $page.params,
-			analyticsId
-		});
-	}
+	import { dev } from '$app/environment';
+  	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
