e17a1a53d8416b16f962fdb8247fa54518da6353
David Runion <david.runion@imedemr.com>
2023-11-11 12:56:06 -0500

    remove vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,8 @@
 	import '@fortawesome/fontawesome-free/css/all.min.css'
 
 	import { dev } from '$app/environment';
-  	import { inject } from '@vercel/analytics';
-	inject({ mode: dev ? 'development' : 'production' });
+  	// import { inject } from '@vercel/analytics';
+	// inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
