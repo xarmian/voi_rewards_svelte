dab0d841c360be2b41c23b7b282afbc22a0f96a0
David Runion <david.runion@imedemr.com>
2024-01-26 00:22:36 -0500

    traits orientation

--- src/lib/component/ui/Token.svelte
+++ src/lib/component/ui/Token.svelte
@@ -10,20 +10,34 @@
         return { trait_type: key, value: token.metadata.properties[key as keyof typeof token.metadata.properties] };
     });
 
+    let propGroups = chunkArray(tokenProps, 5);
+
+    function chunkArray(array: any[], size: number): any[] {
+        let result = [];
+        for (let i = 0; i < array.length; i += size) {
+            result.push(array.slice(i, i + size));
+        }
+        return result;
+    }
+
     let formattedOwner = token.ownerNFD ? token.ownerNFD : token.owner.length > 8
         ? `${token.owner.slice(0, 8)}...${token.owner.slice(-8)}`
         : token.owner;
 </script>
 
 <div class="flex">
-        <img src={token.metadata.image} class="w-48 h-48 mr-3"/>
-        <div class="">
-            <div class="text-2xl font-bold mb-2">{token.metadata.name}</div>
-            <div class="mb-2">Owned by <A href="https://voi.observer/explorer/account/{token.owner}" target="_blank">{formattedOwner}</A></div>
-            <div class="text-md">
-                {#each tokenProps as prop}
-                    <div>{prop.trait_type}: {prop.value}</div>
-                {/each}
-            </div>
+    <img src={token.metadata.image} class="w-48 h-48 mr-3"/>
+    <div class="">
+        <div class="text-2xl font-bold mb-2">{token.metadata.name}</div>
+        <div class="mb-2">Owned by <A href="https://voi.observer/explorer/account/{token.owner}" target="_blank">{formattedOwner}</A></div>
+        <div class="flex flex-wrap">
+            {#each propGroups as group (group)}
+                <div class="mr-5">
+                    {#each group as prop (prop.trait_type)}
+                        <div class="text-md">{prop.trait_type}: {prop.value}</div>
+                    {/each}
+                </div>
+            {/each}
         </div>
+    </div>
 </div>
