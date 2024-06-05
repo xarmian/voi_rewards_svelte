37038fe993fe970c0ee4720b11777fc6941f87e9
xarmian <xarmian@gmail.com>
2024-06-05 19:46:38 -0400

    metadata

--- src/routes/(site)/quests/+page.ts
+++ src/routes/(site)/quests/+page.ts
@@ -34,35 +34,8 @@ export const load = (async () => {
   
     const pageMetaTags = {
         title: 'Voi Testnet Quests',
-        titleTemplate: '%s | Voi Rewards Auditor',
         description: 'Get Your Quest On, with the Voi Testnet Network',
-        openGraph: {
-            url: 'Get Your Quest On, with the Voi Testnet Network',
-            title: 'Voi Testnet Quests',
-            description: 'Get Your Quest On, with the Voi Testnet Network',
-            images: [
-                {
-                    url: 'https://voirewards.com/logos/Voi_Logo_White_on_Purple_Background.png',
-                    width: 192,
-                    height: 192,
-                    alt: 'Voi Logo'
-                },
-                {
-                    url: 'https://voirewards.com/logos/Voi_Logo_White_on_Purple_Background.png',
-                    width: 512,
-                    height: 512,
-                    alt: 'Voi Logo'
-                },
-            ],
-            siteName: 'VoiTestnetQuests'
-        },
-        twitter: {
-            cardType: 'summary',
-            title: 'Voi Testnet Quests',
-            description: 'Get Your Quest On, with the Voi Testnet Network',
-            image: 'https://voirewards.com/logos/Voi_Logo_White_on_Purple_Background.png',
-            imageAlt: 'Voi Logo'
-        }
+        imageUrl: 'https://voirewards.com/logos/Voi_Logo_White_on_Purple_Background.png',
       };
 
 	return {
