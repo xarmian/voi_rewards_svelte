7f2d275e1ba573b008245c5a591a6bd6329c8502
xarmian <xarmian@gmail.com>
2024-10-24 18:43:47 -0400

    replace ecosystem with landing page, move old ecosystem page to /directory

---
+++ src/routes/(site)/directory/[...category]/+page.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { page } from '$app/stores';
+  import { goto } from '$app/navigation';
+  import { fetchProjects } from '../../phase2/[...slug]/projects';
+  import type { IProject } from '$lib/data/types';
+  import ProjectCard from '$lib/components/ecosystem/ProjectCard.svelte';
+	import Share from '$lib/component/ui/Share.svelte';
+
+  export let data;
+
+  let projects: IProject[] = [];
+  let filteredProjects: IProject[] = [];
+  let searchTerm = '';
+  let selectedCategory = data.category;
+
+  // primary sorting for categories
+  const categorySort = [ 'Wallets', 'DEXes', 'NFTs', 'Infrastructure', 'Tools' ];
+
+  onMount(async () => {
+    projects = await fetchProjects();
+    filteredProjects = projects;
+  });
+
+  $: {
+    filteredProjects = projects.filter(project => 
+      project.status === 'active'
+      && (project.title.toLowerCase().includes(searchTerm.toLowerCase())
+      || project.description.toLowerCase().includes(searchTerm.toLowerCase())
+      || project.category.toLowerCase().includes(searchTerm.toLowerCase()))
+      && (
+        selectedCategory === 'All' || 
+        (selectedCategory === 'New' && project.new === true) || 
+        project.category === selectedCategory
+      )
+    ).sort((a, b) => {
+      const indexA = categorySort.indexOf(a.category);
+      const indexB = categorySort.indexOf(b.category);
+      if (indexA === -1 && indexB === -1) return 0;
+      if (indexA === -1) return 1;
+      if (indexB === -1) return -1;
+      return indexA - indexB;
+    });
+  }
+
+  $: categories = ['All', ...new Set(projects.filter(p => p.status === 'active').map(p => p.category))];
+  
+  function handleCategoryClick(event: CustomEvent<string>) {
+    selectedCategory = event.detail;
+    updateURL(selectedCategory);
+  }
+
+  function updateURL(category: string) {
+    const newPath = category === 'All' ? '/ecosystem' : `/ecosystem/${category}`;
+    goto(newPath, { replaceState: true });
+  }
+
+  let url = '';
+  let text = '';
+
+  $: if (selectedCategory !== 'All') {
+    text = `${selectedCategory}? @Voi_Net has that. Check out ${selectedCategory} in the Voi Ecosystem, right here! #Voiagers`;
+    url = data.url.href;
+  }
+  else {
+    text = 'Do you know about the @Voi_Net Ecosystem? Check it out here! #Voiagers';
+    url = data.url.href;
+  }
+</script>
+
+<div class="bg-gradient-to-b from-purple-100 to-white dark:from-gray-900 dark:to-gray-800 min-h-screen py-8 px-4 sm:px-6 lg:px-8 relative">
+  <div class="flex flex-col md:flex-row items-center mt-4 mb-4 justify-between">
+    <span> </span>
+    <h1 class="text-5xl font-bold text-center mb-4">Voi Ecosystem Hub</h1>
+    <div class="text-center md:self-start">
+      <Share {url} {text} />
+    </div>
+  </div>
+  <div class="container mx-auto px-4">
+    
+    <div class="flex flex-col md:flex-row justify-between items-center mb-4">
+      <input
+        type="text"
+        placeholder="Search projects..."
+        bind:value={searchTerm}
+        class="w-full md:w-1/3 px-4 py-2 rounded-lg bg-white dark:bg-gray-700 text-gray-800 dark:text-white mb-4 md:mb-0"
+      />
+      <div class="flex flex-wrap justify-center gap-2">
+        {#each categories as category}
+          <button
+            class="px-4 py-2 rounded-full text-sm font-semibold {selectedCategory === category ? 'bg-purple-600 dark:bg-purple-200 text-white dark:text-purple-800' : 'bg-purple-200 dark:bg-purple-800 text-purple-800 dark:text-purple-200'}"
+            on:click={() => {
+              selectedCategory = category;
+              updateURL(category);
+            }}
+          >
+            {category}
+          </button>
+        {/each}
+        <button
+          class="px-4 py-2 rounded-full text-sm font-semibold {selectedCategory === 'New' ? 'bg-red-600 dark:bg-red-200 text-white dark:text-red-800' : 'bg-red-200 dark:bg-red-800 text-red-800 dark:text-red-200'}"
+          on:click={() => {
+            selectedCategory = 'New';
+            updateURL('New');
+          }}
+        >
+          New
+        </button>
+      </div>
+    </div>
+
+    {#if filteredProjects.length > 0}
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {#each filteredProjects as project (project.id)}
+          <ProjectCard {project} on:categoryClick={handleCategoryClick} />
+        {/each}
+      </div>
+    {:else}
+      <p class="text-center text-2xl mt-16">Projects Loading...</p>
+    {/if}
+  </div>
+</div>
--- src/routes/(site)/ecosystem/[...category]/+page.svelte
+++ src/routes/(site)/ecosystem/[...category]/+page.svelte
@@ -1,122 +1,140 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-  import { page } from '$app/stores';
-  import { goto } from '$app/navigation';
   import { fetchProjects } from '../../phase2/[...slug]/projects';
   import type { IProject } from '$lib/data/types';
-  import ProjectCard from '$lib/components/ecosystem/ProjectCard.svelte';
-	import Share from '$lib/component/ui/Share.svelte';
-
-  export let data;
 
   let projects: IProject[] = [];
-  let filteredProjects: IProject[] = [];
-  let searchTerm = '';
-  let selectedCategory = data.category;
+  const categorySort = ['Wallets', 'DEXes', 'NFTs' ];
 
-  // primary sorting for categories
-  const categorySort = [ 'Wallets', 'DEXes', 'NFTs', 'Infrastructure', 'Tools' ];
+  const categoryDescriptions: Record<string, string> = {
+    Wallets: 'Get started with Voi by downloading a Wallet.',
+    DEXes: 'Trade your assets on decentralized exchanges.',
+    NFTs: 'Explore unique digital assets and collectibles.',
+    Infrastructure: 'Discover the backbone of the Voi ecosystem.',
+    Tools: 'Enhance your experience with these tools.',
+  };
 
   onMount(async () => {
     projects = await fetchProjects();
-    filteredProjects = projects;
   });
 
-  $: {
-    filteredProjects = projects.filter(project => 
-      project.status === 'active'
-      && (project.title.toLowerCase().includes(searchTerm.toLowerCase())
-      || project.description.toLowerCase().includes(searchTerm.toLowerCase())
-      || project.category.toLowerCase().includes(searchTerm.toLowerCase()))
-      && (
-        selectedCategory === 'All' || 
-        (selectedCategory === 'New' && project.new === true) || 
-        project.category === selectedCategory
-      )
-    ).sort((a, b) => {
-      const indexA = categorySort.indexOf(a.category);
-      const indexB = categorySort.indexOf(b.category);
-      if (indexA === -1 && indexB === -1) return 0;
-      if (indexA === -1) return 1;
-      if (indexB === -1) return -1;
-      return indexA - indexB;
-    });
-  }
-
-  $: categories = ['All', ...new Set(projects.filter(p => p.status === 'active').map(p => p.category))];
-  
-  function handleCategoryClick(event: CustomEvent<string>) {
-    selectedCategory = event.detail;
-    updateURL(selectedCategory);
-  }
-
-  function updateURL(category: string) {
-    const newPath = category === 'All' ? '/ecosystem' : `/ecosystem/${category}`;
-    goto(newPath, { replaceState: true });
-  }
-
-  let url = '';
-  let text = '';
-
-  $: if (selectedCategory !== 'All') {
-    text = `${selectedCategory}? @Voi_Net has that. Check out ${selectedCategory} in the Voi Ecosystem, right here! #Voiagers`;
-    url = data.url.href;
-  }
-  else {
-    text = 'Do you know about the @Voi_Net Ecosystem? Check it out here! #Voiagers';
-    url = data.url.href;
-  }
+  $: projectsByCategory = categorySort.reduce((acc, category) => {
+    let filteredProjects = projects.filter(project => project.category === category && project.status === 'active');
+    
+    if (category === 'Wallets') {
+      const walletOrder = ["Kibisis", "Biatec Wallet (A-Wallet)", "Lute"];
+      filteredProjects.sort((a, b) => {
+        const indexA = walletOrder.indexOf(a.title);
+        const indexB = walletOrder.indexOf(b.title);
+        return indexA - indexB;
+      });
+    }
+    
+    acc[category] = filteredProjects;
+    return acc;
+  }, {} as Record<string, IProject[]>);
 </script>
 
-<div class="bg-gradient-to-b from-purple-100 to-white dark:from-gray-900 dark:to-gray-800 min-h-screen py-8 px-4 sm:px-6 lg:px-8 relative">
-  <div class="flex flex-col md:flex-row items-center mt-4 mb-4 justify-between">
-    <span> </span>
-    <h1 class="text-5xl font-bold text-center mb-4">Voi Ecosystem Hub</h1>
-    <div class="text-center md:self-start">
-      <Share {url} {text} />
+<div class="landing-page">
+  <header class="hero parallax bg-purple-900 text-white">
+    <div class="container mx-auto px-4 py-20 text-center">
+      <h1 class="text-6xl font-bold mb-4">Voi Ecosystem Hub</h1>
+      <p class="text-2xl">Discover the future of Community Blockchain</p>
     </div>
-  </div>
-  <div class="container mx-auto px-4">
-    
-    <div class="flex flex-col md:flex-row justify-between items-center mb-4">
-      <input
-        type="text"
-        placeholder="Search projects..."
-        bind:value={searchTerm}
-        class="w-full md:w-1/3 px-4 py-2 rounded-lg bg-white dark:bg-gray-700 text-gray-800 dark:text-white mb-4 md:mb-0"
-      />
-      <div class="flex flex-wrap justify-center gap-2">
-        {#each categories as category}
-          <button
-            class="px-4 py-2 rounded-full text-sm font-semibold {selectedCategory === category ? 'bg-purple-600 dark:bg-purple-200 text-white dark:text-purple-800' : 'bg-purple-200 dark:bg-purple-800 text-purple-800 dark:text-purple-200'}"
-            on:click={() => {
-              selectedCategory = category;
-              updateURL(category);
-            }}
-          >
-            {category}
-          </button>
-        {/each}
-        <button
-          class="px-4 py-2 rounded-full text-sm font-semibold {selectedCategory === 'New' ? 'bg-red-600 dark:bg-red-200 text-white dark:text-red-800' : 'bg-red-200 dark:bg-red-800 text-red-800 dark:text-red-200'}"
-          on:click={() => {
-            selectedCategory = 'New';
-            updateURL('New');
-          }}
-        >
-          New
-        </button>
-      </div>
-    </div>
+  </header>
 
-    {#if filteredProjects.length > 0}
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {#each filteredProjects as project (project.id)}
-          <ProjectCard {project} on:categoryClick={handleCategoryClick} />
-        {/each}
+  {#each categorySort as category, index}
+    <section class={`py-20 ${index % 2 === 0 ? 'bg-white' : 'bg-gray-100'}`}>
+      <div class="container mx-auto px-4">
+        <div class="flex flex-col md:flex-row items-center">
+          <div class="md:w-1/2 mb-8 md:mb-0 {index % 2 === 0 ? 'md:pr-8' : 'md:pl-8 md:order-2'}">
+            <h2 class="text-4xl font-bold text-purple-800 mb-4">{category}</h2>
+            <p class="text-xl text-gray-700 mb-6">{categoryDescriptions[category]}</p>
+          </div>
+          <div class="md:w-1/2 {index % 2 === 0 ? '' : 'md:order-1'}">
+            <ul class="grid grid-cols-2 gap-4">
+              {#each projectsByCategory[category] || [] as project}
+                <li>
+                  <a 
+                    href={project.url} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    class="flex items-center justify-center p-2 rounded-lg hover:bg-purple-100 transition-colors duration-200"
+                  >
+                    {#if project.logo}
+                      <img src={project.logo} alt={project.title} class="mr-2 object-contain h-[{project.title === 'Lute' ? '4rem' : '5rem'}]" />
+                    {:else}
+                      <div class="w-8 h-8 mr-2 bg-purple-600 rounded-full flex items-center justify-center text-white font-bold">
+                        {project.title[0]}
+                      </div>
+                    {/if}
+                  </a>
+                </li>
+              {/each}
+            </ul>            
+          </div>
+        </div>
       </div>
-    {:else}
-      <p class="text-center text-2xl mt-16">Projects Loading...</p>
-    {/if}
-  </div>
+    </section>
+  {/each}
+
+  <!-- Discover more section -->
+  <section class="py-20 bg-purple-100">
+    <div class="container mx-auto px-4 text-center">
+      <h2 class="text-4xl font-bold text-purple-800 mb-6">Discover More</h2>
+      <p class="text-xl text-gray-700 mb-8">Explore the full range of projects and opportunities in the Voi ecosystem.</p>
+      <a 
+        href="/directory" 
+        rel="noopener noreferrer" 
+        class="inline-block bg-purple-600 text-white font-bold py-3 px-6 rounded-lg hover:bg-purple-700 transition-colors duration-200"
+      >
+        Visit the Voi Directory
+      </a>
+    </div>
+  </section>
+
+  <footer class="bg-purple-900 text-white py-10">
+    <div class="container mx-auto px-4 text-center">
+      <p>&copy; 2024 Voi Rewards. All rights reserved.</p>
+    </div>
+  </footer>
 </div>
+
+<style>
+  .landing-page {
+    overflow-x: hidden;
+  }
+
+  .parallax {
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .hero {
+    background-image: url('https://source.unsplash.com/random/1920x1080?blockchain');
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .parallax-image {
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    transition: transform 0.3s ease-out;
+  }
+
+  .parallax-image:hover {
+    transform: scale(1.05);
+  }
+
+  @media (max-width: 768px) {
+    .parallax, .parallax-image {
+      background-attachment: scroll;
+    }
+  }
+</style>
