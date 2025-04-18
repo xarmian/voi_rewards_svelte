dbff76df5dc21a4800e240a88a782fe8928b49bb
xarmian <xarmian@gmail.com>
2025-04-18 10:06:21 -0400

    remove defly test

--- src/routes/(site)/wallet/[...id]/+page.svelte
+++ src/routes/(site)/wallet/[...id]/+page.svelte
@@ -336,7 +336,7 @@
                 </button>
             </div>
             <Web3Wallet
-                availableWallets={['WalletConnect', 'Kibisis', 'LuteWallet', 'BiatecWallet', 'DeflyWallet']}
+                availableWallets={['WalletConnect', 'Kibisis', 'LuteWallet', 'BiatecWallet']}
                 showAuthButtons={false}
                 {algodClient}
                 indexerClient={algodIndexer}
