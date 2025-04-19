97aa68750043f977dd157808df3c7db07a84e816
xarmian <xarmian@gmail.com>
2025-04-19 18:28:05 -0400

    pera test

--- src/routes/(site)/wallet/[...id]/+page.svelte
+++ src/routes/(site)/wallet/[...id]/+page.svelte
@@ -336,7 +336,7 @@
                 </button>
             </div>
             <Web3Wallet
-                availableWallets={['WalletConnect', 'Kibisis', 'LuteWallet', 'BiatecWallet']}
+                availableWallets={['WalletConnect', 'Kibisis', 'LuteWallet', 'BiatecWallet', 'PeraWallet']}
                 showAuthButtons={false}
                 {algodClient}
                 indexerClient={algodIndexer}
@@ -655,7 +655,7 @@
 
             <div class="">
               <Web3Wallet
-                availableWallets={['WalletConnect', 'Kibisis', 'LuteWallet', 'BiatecWallet']}
+                availableWallets={['WalletConnect', 'Kibisis', 'LuteWallet', 'BiatecWallet', 'PeraWallet']}
                 showAuthButtons={false}
                 {algodClient}
                 indexerClient={algodIndexer}
