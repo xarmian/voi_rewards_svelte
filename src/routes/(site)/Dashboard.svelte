a3d18001d5995cd369053e8f16b82be4b6f4d023
xarmian <xarmian@gmail.com>
2025-10-06 11:17:03 -0400

    remove gotvoi ad

--- src/routes/(site)/Dashboard.svelte
+++ src/routes/(site)/Dashboard.svelte
@@ -468,7 +468,7 @@
 		</div>
 	</div>
 
-	<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 md:py-8">
+	<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 md:py-8 hidden">
 		<GotVoiAd />
 	</div>
 
